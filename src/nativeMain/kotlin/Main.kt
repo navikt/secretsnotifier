6fc6643d8ab1a01f07e01515c880a7b8b3fdd425
J-K. Solbakken <jksolbakken@gmail.com>
2023-08-23 14:04:27 +0200

    msg formatting
    switch to http engine that works with tls cross platform

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import io.ktor.client.HttpClient
+import io.ktor.client.engine.curl.Curl
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.cinterop.ExperimentalForeignApi
@@ -25,7 +26,7 @@ fun main() = runBlocking {
         }
     }.forEach { (team, repos) ->
         val heading = ":wave: *Hei, ${team.slug}* :github2:"
-        val msg = "GitHub har oppdaget hemmeligheter i repo som dere er admin i: ${linksTo(repos)}. Klikk på linkene for å se detaljer. Dersom hemmelighetene er aktive må de roteres så fort som mulig, og videre varsling og steg for å avdekke evt. misbruk må iverksettes. Når dette er gjort (eller dersom dette er falske positiver) lukkes varselet ved å velge i nedtrekksmenyen `Close as`."
+        val msg = "GitHub har oppdaget hemmeligheter i repo som dere er admin i:\n\n ${linksTo(repos)}\n\nKlikk på linkene for å se detaljer. Dersom hemmelighetene er aktive må de roteres så fort som mulig, og videre varsling og steg for å avdekke evt. misbruk må iverksettes. Når dette er gjort (eller dersom dette er falske positiver) lukkes varselet ved å velge i nedtrekksmenyen `Close as`.\n\nDu kan også lese mer om håndtering av hemmeligheter i vår <https://sikkerhet.nav.no/docs/sikker-utvikling/hemmeligheter|Security Playbook>"
         val result = slack.send("#jk-tullekanal", heading, msg)
         if (!result.ok) println("Error while notifying ${team.slug} in ${team.slackChannel}: ${result.errorMessage}")
     }
@@ -34,10 +35,10 @@ fun main() = runBlocking {
 
 @OptIn(ExperimentalForeignApi::class)
 private fun envOrDie(name: String): String = getenv(name)?.toKString()
-    ?: throw RuntimeException("Unable to find envvar $name, I'm useless without it")
+    ?: throw RuntimeException("Unable to find env var $name, I'm useless without it")
 
 @OptIn(ExperimentalSerializationApi::class)
-private val httpClient = HttpClient {
+private val httpClient = HttpClient(Curl) {
     expectSuccess = true
     install(ContentNegotiation) {
         json(json = Json {
@@ -48,6 +49,6 @@ private val httpClient = HttpClient {
 }
 
 private fun linksTo(repos: List<RepoWithSecret>) =
-    repos.joinToString(separator = ", ") {
+    repos.joinToString(separator = "\n• ", prefix = "• ") {
         "<https://github.com/${it.fullName}/security/secret-scanning|${it.name()}>"
     }
