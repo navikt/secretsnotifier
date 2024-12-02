49dce8d9429de67c8fce2718c3016172d176a960
J-K. Solbakken <jksolbakken@gmail.com>
2024-12-02 11:02:43 +0100

    send to the actual slack channels

--- src/main/kotlin/no/nav/Main.kt
+++ src/main/kotlin/no/nav/Main.kt
@@ -31,7 +31,7 @@ fun main() = runBlocking {
             val heading = ":wave: *Hei, ${owner.slug}* :github2:"
             val msg =
                 "GitHub har oppdaget hemmeligheter i repo som dere eier:\n\n ${linkTo(repo)}\n\n Dersom hemmelighetene er aktive må de *roteres* så fort som mulig, og videre varsling og steg for å avdekke evt. misbruk må iverksettes. \n\n :warning: Husk at Git aldri glemmer, så kun fjerning fra koden er IKKE tilstrekkelig.\n\nNår dette er gjort (eller dersom dette er falske positiver) lukkes varselet ved å velge i nedtrekksmenyen `Close as`.\n\nDu kan også lese mer om håndtering av hemmeligheter i vår <https://sikkerhet.nav.no/docs/sikker-utvikling/hemmeligheter|Security Playbook>"
-            slack.send("#jk-tullekanal", heading, msg)
+            slack.send(owner.slackChannel, heading, msg)
         } ?: println("Unable to find an owner for ${repo.fullName}")
     }
 
